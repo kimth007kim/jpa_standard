b2e2995b6c075cfd11f6a2041210da6dd48042d9
kimth007kim <kimth007kim@naver.com>
2021-08-04 19:00:57 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편/ 영속성 관리 - 내부 동작 방식 /영속성 컨텍스트 1

--- study/ex-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ study/ex-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,20 +18,16 @@ public class JpaMain {
         EntityTransaction tx= em.getTransaction();
         tx.begin();
 
-
         try{
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA");
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            for (Member member: result) {
-                System.out.println("member.name = "+ member.getName());
-
-            }
-
+            //영속
+            System.out.println("====== BEFORE ======");
+            em.persist(member);
+            System.out.println("====== AFTER ======");
             tx.commit();
 
         }catch (Exception e){
